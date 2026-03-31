aca074b6e1adf903638a39b268bfcbe25420ec9c
Chukwuani Stephen <chukwuanisteven@gmail.com>
2026-03-31 13:56:14 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+
+  // Remove the newline character if present
+  command[strcspn(command, "\n")] = '\0';
+  printf("%s: command not found\n", command);
 
   return 0;
 }
