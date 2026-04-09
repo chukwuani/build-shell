9f9c94eac8b077ea05d9a572d50d04f700c0cc33
Chukwuani Stephen <chukwuanisteven@gmail.com>
2026-04-09 15:44:43 +0100

    FEAT: check executeable paths

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  puts("### Shell Version 1.0.0 ###");
+  puts("Welcome to my shell! Type 'exit' to quit.\n");
+
   // Infinite loop to read commands from the user
   while (1)
   {
-    puts("Welcome to my shell! Type 'exit' to quit.");
-    fputs("Hello World", stdout);
+    fputs("> ", stdout);
 
     // Read a line of input from the user
     char command[1024];
@@ -41,7 +44,40 @@ int main(int argc, char** argv)
       }
       else
       {
-        printf("%s: not found\n", arg);
+        // We would search the PATH environment variable for the command
+        const char *pathEnv = getenv("PATH");
+        char *pathCopy = strdup(pathEnv);
+        char fullPath[1024];
+        int found = 0;
+
+        // Tokenize the PATH variable and check each directory for the executable
+        char *dir = strtok(pathCopy, ";");
+        while (dir != NULL)
+        {
+          // Strip trailing backslash from dir if present
+          int len = strlen(dir);
+          if (dir[len - 1] == '\\')
+          dir[len - 1] = '\0';
+          
+          snprintf(fullPath, sizeof(fullPath), "%s\\%s.exe", dir, arg);
+
+          // Check if fullpath is executable
+          if (_access(fullPath, 0) == 0)
+          {
+            printf("%s is %s\n", arg, fullPath);
+            found = 1;
+            break;
+          }
+
+          dir = strtok(NULL, ";");
+        }
+
+        if (!found)
+        {
+          fprintf(stderr, "Error: '%s' is not executable or not found in PATH\n", arg);
+        }
+
+        free(pathCopy);
       }
     }
     else
