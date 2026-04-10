7e97128d59fa0bfd8aa6a176d4952b73b46a1675
Chukwuani Stephen <chukwuanisteven@gmail.com>
2026-04-10 17:22:46 +0100

    FEAT: handle single quotes

--- main.c
+++ main.c
@@ -38,6 +38,51 @@ int main(int argc, char **argv)
       char arg[MAX_INPUT_SIZE];
       strcpy(arg, input + 5);
 
+      // Check if the argument is enclosed in single quotes
+      if (arg[0] == '\'' && arg[strlen(arg) - 1] == '\'')
+      {
+        // Remove the single quotes from the back
+        // Copy the remain content except the first quote back into arg
+        arg[strlen(arg) - 1] = '\0';
+        memmove(arg, arg + 1, strlen(arg));
+      }
+      else
+      {
+        // Else split by delimeter " " and reconstruct the string with spaces in between
+        char *temp = strdup(arg);
+        char result[MAX_INPUT_SIZE] = "";
+
+        char *token = strtok(temp, " ");
+
+        while (token != NULL)
+        {
+          strcat(result, token);
+          strcat(result, " ");
+          token = strtok(NULL, " ");
+        }
+
+        strcpy(arg, result);
+        free(temp);
+      }
+
+      // Split by delimeter '\'' and reconstruct the string
+      char *temp = strdup(arg);
+      char result[MAX_INPUT_SIZE] = "";
+
+      char *token = strtok(temp, "\'");
+
+      while (token != NULL)
+      {
+        trimTrailing(token);   
+        strcat(result, token);
+        token = strtok(NULL, "\'");
+      }
+
+      strcpy(arg, result);
+      free(temp);
+
+      // Remove trailing spaces from the argument
+      trimTrailing(arg);
       // Print the argument after "echo "
       printf("%s\n", arg);
     }
@@ -71,12 +116,18 @@ int main(int argc, char **argv)
       char dir[MAX_INPUT_SIZE];
       strcpy(dir, input + 3);
 
-      if (chdir(dir) != 0)
+      // Check for "~" and replace with home directory
+      if (strcmp(dir, "~") == 0)
       {
-        fprintf(stderr, "%s: No such file or directory\n", dir);
-      }
-    }
+        char *homeDir = getenv("HOME");
 
+        if (homeDir != NULL)
+          strcpy(dir, homeDir);
+      }
+
+      if (chdir(dir) != 0)
+        fprintf(stderr, "%s: No such file or directory\n", dir);
+    }
     else
     {
       char *token = strtok(input, " \t");
